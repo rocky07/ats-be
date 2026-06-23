67a7f359798b7085c6d434f381298047e430e80c
rocky <ramesan.rakesh@gmail.com>
2026-06-22 21:10:48 -0500

    update

--- server.js
+++ server.js
@@ -11,6 +11,10 @@ initDatabase().then(() => {
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
-	console.log('app started , listening on port ${PORT}')
+// app.listen(PORT, () => {
+// 	console.log('app started , listening on port ${PORT}')
+// });
+
+app.listen(PORT, '0.0.0.0', () => {
+    console.log(`Application is running on port ${PORT}`);
 });
